baf541d1590be0afaa866f7ba01e22b0ca1ae151
epikstik <88759413+epikstik@users.noreply.github.com>
2021-08-17 14:46:39 +0300

    Add files via upload

---
+++ epic.py
@@ -0,0 +1,44 @@
+print("helo bois, welkom to da calculator: ")
+no1 = int(input("enter a number: \n"))
+no2 = int(input("enter another number: \n"))
+operator = input("enter a operator (+ , -, /, *): \n")
+
+if(operator == "+"):
+    print("your answer is: \n", no1 + no2)
+    exit()
+
+else:
+          print("wtf u enter")
+if(operator == "-"):
+          print("your answer is: \n", no1 - no2)
+          exit()
+else:
+          print("wtf u enter")
+if(operator == "/"): 
+         print("your answer is: \n", no1 / no2)
+         exit()
+
+else:
+          print("wtf u enter")
+if(operator == "*"):
+    print("i like multiplaction but your answer is: \n", no1 * no2)
+
+if(operator == "TOPSECRET"): 
+    print("Secret System Aquired")
+    print("Would you like to press Secret Button")
+    if(input == "Yes"): 
+        print("Deleting System32.........")
+        print("Succussfully deleted....ERENO{!#} PROCESS 'B***{{{{!*'..")
+        print("Deleting C: .....")
+        print("Succussfully deleted C: ")
+    if(input == "No"): 
+        print("Are you sure you do not want to access Secret Button?")
+        if(input == "No"):
+            ask = input("Would you like to press the button?")
+            if (ask == "Yes"):
+             print("Deleting System32.........")
+            print("Succussfully deleted....ERENO{!#} PROCESS 'B***{{{{!*'..")
+            print("Deleting C: .....")
+            print("Succussfully deleted C: ")
+        if(ask == "Yes"):
+            print("Didn't press button..")
---
+++ main.py
@@ -0,0 +1,25 @@
+# print and var phase
+
+first_name = "epik"
+last_name = "robot"
+full_name = first_name + " " + last_name
+robot_age_digit_1 = 2
+robot_age_digit_2 = 1
+robot_age = 21
+robot_age_real = robot_age + 1
+
+
+print("robot epik      yeree")
+print("gonna print robot name here: "+first_name)
+print("robot gonna do full name reveal")
+print("3")
+print("2")
+print("1")
+print(full_name)
+if(input == "SecretCode"):
+    print("Well, Well, Well")
+    print("Hello there! GET OUTATA HERE M888 I AM SO EPIK I AM PROGRAMMED TO DESTROY YOU!!!!!")
+    if(input == "What do we have here!"):
+        print("How did you access two secret codes!")
+        if(input == "Hax"):
+            print("Restart Console because there is nothing here")
